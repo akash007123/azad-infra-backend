e8358d63f03f4c85c26a79a2a2bab0f0475a6203
Akash Raikwar <akashraikwar.mongoosetech@gmail.com>
2025-11-07 00:40:54 +0530

    shdgd

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 
 // CORS middleware (allowing specific origins for credentials)
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'https://azadinfrastructure.sosapient.in/']; // Add your frontend URLs
+  const allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'https://azadinfrastructure.sosapient.in']; // Add your frontend URLs
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
